fe996f77ff402146540ad3a180e7c0b34c920988
Joseph Ruff <joegruff16@gmail.com>
2024-03-20 17:40:45 -0400

    Fixed broken style sheet links for CSS and JS with HTML

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+// GIVEN an employee payroll tracker
+// WHEN I click the "Add employee" button
+// THEN I am presented with a series of prompts asking for first name, last name, and salary
+// WHEN I finish adding an employee
+// THEN I am prompted to continue or cancel
+// WHEN I choose to continue
+// THEN I am prompted to add a new employee
+// WHEN I choose to cancel
+// THEN my employee data is displayed on the page sorted alphabetically by last name, and the console shows computed and aggregated data
+
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
@@ -23,6 +33,7 @@ const collectEmployees = function () {
 // Display the average salary
 const displayAverageSalary = function (employeesArray) {
   // TODO: Calculate and display the average salary
+
 
 }
 
